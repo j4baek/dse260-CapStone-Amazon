fb6ce557556a9e59e513ac9f0c68a3696fda2a0c
justin-shamoun <justin.shamoun@zodiac.com>
2020-02-08 20:45:04 -0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # dse260-CapStone-Amazon
 UCSD Capstone project with Amazon customer dataset. 
+
+- [Wiki Page](https://sites.google.com/a/eng.ucsd.edu/capstone-2020-amazondata/)
+
+## Installation
+- Install spark
+- requirments.txt
+- aws cli (optional)
+- Neo4J DB
+
+## Usage
+
+
+## Resources
+
+## Refrences
---
+++ src/bin/copyToS3.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+# require credentials to be set up in ~/.aws/credentials file using aws access key and secret.
+# [ucsd]
+# aws_access_key_id=
+# aws_secret_access_key=
+
+
+LOCAL_BUCKET = './data/'
+S3_BUCKET = 's3://dse-cohort5-group1/'
+
+# we currently only have access to us-east-1 (virginia)
+AWS_REGION = 'us-east-1'
+
+# copies data from local computer to s3. Can also be done with python Boto3 sdk
+aws s3 cp $LOCAL_BUCKET $S3_BUCKET --recursive --profile=ucsd --region=us-east-1
+
