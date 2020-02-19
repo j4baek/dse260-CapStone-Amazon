44d5d25b7c0760b48a0be0255c6c304d67fb4635
JH Baek <jh.baek@hp.com>
2020-02-18 20:52:58 -0800

    fix space issue
    Signed-off-by: JH Baek <jh.baek@hp.com>

--- src/bin/copyToS3.sh
+++ src/bin/copyToS3.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # require credentials to be set up in ~/.aws/credentials file using aws access key and secret.
 # [ucsd]
@@ -6,12 +6,11 @@
 # aws_secret_access_key=
 
 
-LOCAL_BUCKET = './data/'
-S3_BUCKET = 's3://dse-cohort5-group1/'
+LOCAL_BUCKET='./data/'
+S3_BUCKET='s3://dse-cohort5-group1/'
 
 # we currently only have access to us-east-1 (virginia)
-AWS_REGION = 'us-east-1'
+AWS_REGION='us-east-1'
 
 # copies data from local computer to s3. Can also be done with python Boto3 sdk
 aws s3 cp $LOCAL_BUCKET $S3_BUCKET --recursive --profile=ucsd --region=us-east-1
-
